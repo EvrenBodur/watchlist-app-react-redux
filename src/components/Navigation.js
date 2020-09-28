ffd0d2c39d4f26366a0217a9f039dc2d5f6884ef
Evren <evrnbdr@gmail.com>
2020-09-28 22:14:31 +0300

    css improvement

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,24 +3,29 @@ import { Link } from "react-router-dom";
 import "../styles/Navigation.css";
 
 const Navigation = () => {
+  const menuHandler = () => {
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("open");
+  };
+
   return (
     <div className="navigation-container">
-      <div className="hamburger-menu">
+      <div onClick={menuHandler} className="hamburger-menu">
         <div className="line"></div>
         <div className="line"></div>
         <div className="line"></div>
       </div>
       <div className="nav-links">
-        <Link className="nav-link" to="/">
+        <Link className="nav-link" to="/" onClick={menuHandler}>
           <i className="fas fa-film"></i> Movies
         </Link>
-        <Link className="nav-link" to="/series">
+        <Link className="nav-link" to="/series" onClick={menuHandler}>
           <i className="fas fa-tv"></i> Tv Series
         </Link>
-        <Link className="nav-link" to="/search">
+        <Link className="nav-link" to="/search" onClick={menuHandler}>
           <i className="fas fa-search"></i> Search
         </Link>
-        <Link className="nav-link" to="/watchlist">
+        <Link className="nav-link" to="/watchlist" onClick={menuHandler}>
           <i className="far fa-list-alt"></i> Watch List
         </Link>
       </div>
